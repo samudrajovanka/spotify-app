e6e29156d3adbf85cce99321074f85644567e33d
Jovanka Samudra <samudrajovanka20@gmail.com>
2022-03-24 11:44:41 +0700

    fix: change to props

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import dataAlbum from '../../data/dataAlbum';
 import './index.css';
+import PropTypes from 'prop-types';
 
-export default function Card() {
+export default function Card({ imageUrl, title, artist }) {
   return (
     <div className="card">
-      <img src={dataAlbum.album.images[0].url} alt={dataAlbum.album.name} className="card__image" />
+      <img src={imageUrl} alt={title} className="card__image" />
       
-      <h3 className="card__album">{dataAlbum.name}</h3>
-      <p className="card__artist">{dataAlbum.artists[0].name}</p>
+      <h3 className="card__title">{title}</h3>
+      <p className="card__artist">{artist}</p>
       
       <div className="button_wrapper">
         <button className="card__button">Select</button>
       </div>
     </div>
   );
+}
+
+Card.propTypes = {
+  imageUrl: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  artist: PropTypes.string.isRequired,
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import dataSong from '../data/dataSong';
 import Card from '../components/Card'
 
 export default function Home() {
   return (
     <main className="container">
       <div className="cards">
-        <Card />
+        <Card
+          imageUrl={dataSong.album.images[0].url}
+          title={dataSong.name}
+          artist={dataSong.artists[0].name}
+        />
       </div>
     </main>
   )
