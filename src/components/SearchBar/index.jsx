98d96a8c3cbf1702d2a58f77d9c39badd17fa42d
Jovanka Samudra <samudrajovanka20@gmail.com>
2022-04-01 01:27:32 +0700

    fix: remove unused comment

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -3,50 +3,6 @@ import config from '../../lib/config';
 import Button from '../Button';
 import './index.css';
 import PropTypes from 'prop-types';
-
-// export default function SearchBar({ accessToken, onSuccess }) {
-//   const [text, setText] = useState('');
-
-//   const handleInput = (e) => {
-//     setText(e.target.value);
-//   }
-
-//   const onSubmit = async (e) => {
-//     e.preventDefault();
-
-//     const requestOptions = {
-//       headers: {
-//         'Authorization': 'Bearer ' + accessToken,
-//         'Content-Type': 'application/json',
-//       },
-//     };
-
-//     try {
-//       const response = await fetch(`${config.SPOTIFY_BASE_URL}/search?type=track&q=${text}`, requestOptions)
-//         .then((data) => data.json());
-
-//       const tracks = response.tracks.items;
-//       onSuccess(tracks);
-//     } catch (e) {
-//       alert(e);
-//     }
-//   }
-
-//   return (
-//     <form className="form-search" onSubmit={onSubmit}>
-//         <input
-//           type="text"
-//           placeholder="Search..."
-//           className="form-search__input"
-//           required
-//           onChange={handleInput}
-//         />
-//         <Button type="submit">Search</Button>
-//       </form>
-//   )
-// }
-
-
 export default function SearchBar({ accessToken, onSuccess, onClearSearch }) {
   const [text, setText] = useState('');
   const [isClear, setIsClear] = useState(true);
