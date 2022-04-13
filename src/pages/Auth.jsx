340b8db7c8f5e5bc5f138651c4173125d3b59224
Jovanka Samudra <samudrajovanka20@gmail.com>
2022-04-14 01:47:19 +0700

    fix: setting host in config

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -45,7 +45,7 @@ export default function Auth() {
     return 'https://accounts.spotify.com/authorize?' +
       `client_id=${clientId}` +
       '&response_type=token' +
-      '&redirect_uri=http://localhost:3000' +
+      `&redirect_uri=${config.HOST}` +
       `&state=${state}` +
       `&scope=${config.SPOTIFY_SCOPE}`;
   }
